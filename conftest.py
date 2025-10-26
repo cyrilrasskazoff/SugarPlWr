36a43a5b3a7ae1a5182de5425e17f8ff69eb26c2
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2025-10-26 13:02:36 +0300

    first commit

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pom_project.pages.dashboard_page_with_locators import DashboardPageWithLocators
+from pom_project.pages.login_page import LoginPage
+from pom_project.pages.dashboard_page import DashboardPage
+from pom_project.pages.login_page_with_locators import LoginPageWithLocators
+
+
+@pytest.fixture
+def login_page(page):
+    return LoginPage(page)
