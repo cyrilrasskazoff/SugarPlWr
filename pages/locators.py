de9c355a6ee1c3b2616e31489198c7c064a4826e
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2025-11-11 21:05:38 +0300

    added header_component.py

--- pages/locators.py
+++ pages/locators.py
@@ -8,6 +8,22 @@ class SidebarLocators:
     HAMBURGER = "button[aria-label='Open']"
     ACCOUNTS_LINK = "a[href='#Accounts']"
 
+class HeaderLocators:
+    LOGO = ("role", "img", "SugarCRM")
+    GLOBAL_SEARCH_INPUT = ("role", "textbox", "Global Search")
+    GLOBAL_SEARCH_BUTTON = ("role", "button", "Run Global Search")
+    NOTIFICATIONS_BUTTON = ("role", "button", "Notifications")
+    AVATAR = ("role", "button", "User Menu")
+    PROFILE_LINK = "a[href='#profile']"
+    CHANGE_PASSWORD_LINK = ("role", "link", "Change Password")
+    EMPLOYEES_LINK = ("role", "link", "Employees")
+    MOBILE_LINK = ("role", "button", "Mobile")
+    SHORTCUTS_LINK = ("role", "button", "Shortcuts")
+    ABOUT_LINK = ("role", "link", "About")
+    LOG_OUT_LINK = ("role", "link", "Log Out")
+
+
+
 
 
 class LoginPageLocators:
